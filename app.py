8448a5e06046ae586743bbbba68f391311ca9f6d
Chris Swindell <chrisswindell@gmail.com>
2024-10-28 09:46:38 -0600

    Added file dialog default

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from processors import NMStateOrderProcessor, FederalOrderProcessor
+import os
 import tkinter as tk
 from tkinter import filedialog
 
+from processors import FederalOrderProcessor, NMStateOrderProcessor
+
 
 def process_order(order_group: str):
-    order_form = filedialog.askopenfilename()
+    # Get the downloads folder path based on the operating system
+    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+
+    order_form = filedialog.askopenfilename(
+        initialdir=downloads_path,
+        title="Select Order Form",
+        filetypes=(("Excel files", "*.xlsx"), ("All files", "*.*")),
+    )
+
     if order_form:
-        if order_group == 'New Mexico State Orders':
+        if order_group == "New Mexico State Orders":
             order_processor = NMStateOrderProcessor(order_form)
         else:  # Federal Orders
             order_processor = FederalOrderProcessor(order_form)
@@ -19,12 +29,16 @@ root.geometry("250x250")
 root.title("Order Processor")
 
 order_type = tk.StringVar()
-order_type.set('New Mexico State Orders')  # default value
+order_type.set("New Mexico State Orders")  # default value
 
-order_type_option = tk.OptionMenu(root, order_type, 'New Mexico State Orders', 'Federal Orders')
+order_type_option = tk.OptionMenu(
+    root, order_type, "New Mexico State Orders", "Federal Orders"
+)
 order_type_option.pack()
 
-process_button = tk.Button(root, text="Process Order", command=lambda: process_order(order_type.get()))
+process_button = tk.Button(
+    root, text="Process Order", command=lambda: process_order(order_type.get())
+)
 process_button.pack()
 
 root.mainloop()
